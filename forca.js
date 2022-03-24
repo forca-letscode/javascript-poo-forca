c54d06f944cf7151033d317de8c59cf6217db926
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-03-23 22:58:10 -0300

    Código Refatorado

--- forca.js
+++ forca.js
@@ -10,20 +10,16 @@ class Usuario {
 };
 
 function iniciaJogo() {
-    //  Inserir validação nas entradas abaixo.
-    //  TODO ITEM #5
-    //  ITEM #1
+    //  ITEM #1 e #5
     while (true){
         const nome = prompt("Digite seu nome:");
         const email = prompt("Digite seu email:");
-        if (nome != '' && email != ''){
+        if ((nome != '' && email != '') && (nome != null && email != null)){
             const jogador = new Usuario(nome, email);
             break;
-        } else { alert('Nome ou e-mail, invalido.')}
+        } else { alert('Nome ou e-mail inválido.')}
     }
     
-    
-
     //  ITEM #3
     const palavras = {
         educacao: ['escola', 'biblioteca', 'professor'],
@@ -31,13 +27,12 @@ function iniciaJogo() {
         meio_ambiente: ['ecossistema', 'fauna', 'flora']
     };
 
-    //  Inserir validação nas entradas abaixo.
-    //  TODO ITEM #5
+    //  ITEM #5
     while (true){
         const tema = prompt("Qual tema deseja jogar?\n1 - EDUCAÇÃO\n2 - SAÚDE\n3 - MEIO AMBIENTE");
         if (tema == 1 || tema == 2 || tema == 3){
             break;
-        } else { alert('Opção invalida!!! digite 1, 2 ou 3. ')}
+        } else { alert('Opção invalida!!! Digite 1, 2 ou 3. ')}
     }
     
     
@@ -59,11 +54,9 @@ function iniciaJogo() {
 
     let erro = 0;
     let acertou = false;
-
     while(erro < 7) {
         //  Inserir validação nas entradas abaixo.
         //  TODO ITEM #5
-        
         while (true){
             let letra = prompt('Digite uma letra:').toLowerCase();
             if (letra.length == 1){
@@ -71,21 +64,24 @@ function iniciaJogo() {
                 break;
             } else { alert('Digite um caracter de a-z')}
         }
+
         console.log('letra:', letra);
-        for(let i = 0; i < forca.length; i++) {
-            if(forca[i] == letra) {
+        for(let i = 0; i < forca.length; i++) {  // percorre a palavra letra por letra
+
+            if(forca[i] == letra) {  // compara se a palavra contém a letra digitada
                 console.log('acertou', letra);
-                forca2[i] = letra;
-                acertou = true;
-                if(forca.join("") == forca2.join("")) erro = 8;
+                forca2[i] = letra;  //  adiciona a letra correta e no local correto
+                acertou = true;  // flag para não contabilizar erro
+                if(forca.join("") == forca2.join("")) erro = 8;  // verifica se a palavra está completa e finaliza
                 console.log('forca2:', forca2);
             }
         }
+
         if(acertou == false) {
             console.log("Você errou!");
             console.log('Você usou: ', ++erro, " de 7 tentativas");
         }    
-        acertou = false;    
+        acertou = false;    //  reseta a flag de erro
     }
 
     if(forca.join("") == forca2.join("")) {
@@ -107,5 +103,8 @@ while(jogar) {  //  Implicitamente: jogar == true
     //  Inserir validação na entrada abaixo.
     //  TODO ITEM #5
     const resposta = prompt("Deseja jogar novamente?\n1 - SIM\n2 - NÃO");
-    if (resposta == 2) jogar = false;
+    if (resposta == 2 ) break;
+    else if (resposta == 1) continue;
+    else alert('Opção invalida!!! Digite 1, 2 ou 3. ');
+    // if (resposta == 2) jogar = false;
 };
